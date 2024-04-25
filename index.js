18106c05faef39f0f4ba7d6df8d485369fc2b0a6
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-04-25 17:07:36 +0200

    added a function try and catch, with a fecth, to fetch weather data based on current geolaction.

--- index.js
+++ index.js
@@ -48,3 +48,27 @@ function getCurrentTime() {
 //Update the current time based on current geolocation every second
 
 setInterval(getCurrentTime, 1000)
+
+//Fetch weather data based on current geolocation
+
+navigator.geolocation.getCurrentPosition(async position => {
+    try {
+        const res = await fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        if (!res.ok) {
+            throw Error("Weather data not available")
+            }
+            const data = await res.json()
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
+            `
+
+        } catch (err) {
+            console.error(err)
+        }
+        
+        
+    });
+
