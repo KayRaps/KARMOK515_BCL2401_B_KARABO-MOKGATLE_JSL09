06c0baefd80e32334dc655fa5914d7febf5bbb10
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-04-25 17:04:27 +0200

    added a code that fetches dogcoin information, to be displayed on the dashboard, got the current market data. Used a catch function to catch an error and then console the error. And I fetched the id with a .innerHTML.

--- index.js
+++ index.js
@@ -22,3 +22,19 @@ try {
         if (!res.ok) {
             throw Error("Something went wrong")
     }
+    const data = await res.json()
+     //Display Dogecoin data in the HTML
+     document.getElementById("crypto-top").innerHTML = `
+     <img src=${data.image.small} />
+     <span>${data.name}</span>
+     
+   `
+   document.getElementById("crypto").innerHTML += `
+           <p>🎯: $${data.market_data.current_price.usd}</p>
+           <p>👆: $${data.market_data.high_24h.usd}</p>
+           <p>👇: $${data.market_data.low_24h.usd}</p>
+   `
+   
+   } catch (err) {
+       console.error(err)
+}
